8aa87eb60dd0e81854360c92870cd55e11309ace
Denys Liutyi <ashuksu@bonicoinsights.com>
2025-04-22 21:25:18 +0000

    added 2 images, button and functionality for toggle images

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -5,6 +5,16 @@ sap.ui.define([
 
     return Controller.extend("projectc.controller.Main", {
         onInit() {
+        },
+        clickMyButton() {
+            let oView = this.getView();
+            let oImage1 = oView.byId('myImage1');
+            let oImage2 = oView.byId('myImage2');
+
+            let isImage1Visible = oImage1.getVisible();
+
+            oImage1.setVisible(!isImage1Visible);
+            oImage2.setVisible(isImage1Visible);
         }
     });
 });
