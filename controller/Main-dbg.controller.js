59437472011372a4114d0adb57a0c6e4424e4ac6
Denys Liutyi <ashuksu@bonicoinsights.com>
2025-04-27 20:16:13 +0000

    Updates

--- controller/Main-dbg.controller.js
+++ controller/Main-dbg.controller.js
@@ -8,6 +8,7 @@ sap.ui.define([
         },
         clickMyButton() {
             let oView = this.getView();
+
             let oImage1 = oView.byId('myImage1');
             let oImage2 = oView.byId('myImage2');
 
@@ -15,6 +16,40 @@ sap.ui.define([
 
             oImage1.setVisible(!isImage1Visible);
             oImage2.setVisible(isImage1Visible);
-        }
+
+            let text = {on: 'Toggle ON', off: 'Toggle OFF'};
+            let oToggleButton = oView.byId('myToggleButton');
+
+            if (oToggleButton.getPressed()) {
+                oToggleButton.setPressed(false);
+                sap.m.MessageToast.show(text.off);
+                oToggleButton.setText(text.off);
+            }
+        },
+        toggleMyButton() {
+            let oView = this.getView();
+            
+            let oToggleButton = oView.byId('myToggleButton');
+            let bPressed = oToggleButton.getPressed(); // is active
+
+            let text = {on: 'Toggle ON', off: 'Toggle OFF'};
+            
+            if (bPressed) {
+                sap.m.MessageToast.show(text.on);
+                oToggleButton.setText(text.on);
+            } else {
+                sap.m.MessageToast.show(text.off);
+                oToggleButton.setText(text.off);
+            }
+
+            let oImage1 = oView.byId('myImage1');
+            let oImage2 = oView.byId('myImage2');
+
+            let isImage1Visible = oImage1.getVisible();
+            let isImage2Visible = oImage2.getVisible();
+
+            oImage1.setVisible(bPressed);
+            oImage2.setVisible(bPressed);
+        },
     });
 });
